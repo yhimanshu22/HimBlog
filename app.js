8fd0178e9cec2e1a841828c09cd88a82c6ad7676
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-01-07 03:29:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const Blog = require("./models/blog");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+PORT = 8000 ;
+MONGO_URL = 'mongodb+srv://<username>:<password>@cluster0.aduicuj.mongodb.net/'
+
+mongoose
+  .connect(MONGO_URL)
+  .then((e) => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
